01c1ce37e92de82daf967b1b2095db763be66564
isakcmz <isafurkankacmaz@gmail.com>
2026-07-24 21:07:01 +0300

    Manuel Mapleme işlemi ve General Mapping işlemi ve AutoMapper

---
+++ APIproject.WebApi/Dtos/FeatureDtos/CreateFeatureDto.cs
@@ -0,0 +1,11 @@
+﻿namespace APIproject.WebApi.Dtos.FeatureDtos
+{
+    public class CreateFeatureDto
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public string VideoURL { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ APIproject.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using APIproject.WebApi.Entities;
+using APIproject.WebApi.Dtos.FeatureDtos;
+using APIproject.WebApi.Dtos.MessageDtos;
+
+namespace APIproject.WebApi.Mapping
+{
+    public class GeneralMapping: Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature, GetByIdFeatureDto>().ReverseMap();
+
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Message, CreateMessageDto>().ReverseMap();
+            CreateMap<Message, UpdateMessageDto>().ReverseMap();
+            CreateMap<Message, GetByIdMessageDto>().ReverseMap();
+        }
+    }
+}
