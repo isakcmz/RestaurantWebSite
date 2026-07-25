1c496297331ff0ba874bd75bd4d1f3155dc805c1
isakcmz <isafurkankacmaz@gmail.com>
2026-07-25 13:48:36 +0300

    View Component Kullanımı (Head ve Navbar)

---
+++ APIproject.WebUI/ViewComponents/_NavbarDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIproject.WebUI.ViewComponents
+{
+    public class _NavbarDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
