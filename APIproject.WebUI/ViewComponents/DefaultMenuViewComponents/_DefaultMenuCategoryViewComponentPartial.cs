15878fff60a63bf244dddbc3789d084fa906831a
isakcmz <isafurkankacmaz@gmail.com>
2026-07-27 22:13:42 +0300

    Kategorilerin UI'da Consume işlemi

---
+++ APIproject.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuCategoryViewComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using APIproject.WebUI.Dtos.CategoryDtos;
+using APIproject.WebUI.Dtos.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace APIproject.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuCategoryViewComponentPartial : ViewComponent
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultMenuCategoryViewComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+ 
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7042/api/Categories/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ APIproject.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIproject.WebUI.ViewComponents.DefaultMenuViewComponentPartial
+{
+    public class _DefaultMenuViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
